ecbdb158e5ad5ffe54589490979806834117df70
Štěpán Zoubek <stepan.zoubek@gmail.com>
2020-08-03 21:58:31 +0200

    Update file structure, add routes

--- src/app.js
+++ src/app.js
@@ -21,7 +21,17 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(publicDirectoryPath))
 
+app.get('/', (req, res)=>{
+    res.render('home')
+})
 
+app.get('/login', (req, res)=>{
+    res.render('login')
+})
+
+app.get('/register', (req, res)=>{
+    res.render('register')
+})
 
 
 
